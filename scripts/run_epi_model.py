3b2cb4604d73c36a26024a0dc9e9038cf096cbc6
will-s-hart <william.hart@maths.ox.ac.uk>
2025-06-17 14:37:22 +0100

    Run model one realization at a time to avoid memory issues

--- scripts/run_epi_model.py
+++ scripts/run_epi_model.py
@@ -1,7 +1,9 @@
 import argparse
 import pathlib
+import tempfile
 
 import dask.diagnostics
+import dask.distributed
 import xarray as xr
 from climepi import epimod
 from inputs import DATASETS, EPI_MODEL_NAME
@@ -17,15 +19,32 @@ def _run_epi_model(
     )
     ds_clim.time_bnds.load()
     epi_model = epimod.get_example_model(EPI_MODEL_NAME)
-    with dask.diagnostics.ProgressBar():
-        ds_epi = (
-            ds_clim.climepi.run_epi_model(
-                epi_model, return_yearly_portion_suitable=True
-            ).compute()  # For some reason next step stalls if not computed here (may be
-            # fixed in next xcdat version; using Dask distributed scheduler may also
-            # help)
+    temp_dir = tempfile.TemporaryDirectory(suffix="_climepi")
+    for realization in ds_clim.realization.values:
+        print(f"Running epi model for realization: {realization}")
+        delayed_obj = (
+            ds_clim.sel(realization=[realization])
+            .climepi.run_epi_model(epi_model, return_yearly_portion_suitable=True)
+            .to_netcdf(
+                pathlib.Path(temp_dir.name) / f"{realization}.nc",
+                compute=False,
+            )
         )
-    ds_epi.to_netcdf(pathlib.Path(__file__).parents[1] / f"results/{dataset}.nc")
+        with dask.diagnostics.ProgressBar():
+            delayed_obj.compute()
+    print("Combining results from all realizations")
+    ds_epi = xr.open_mfdataset(
+        str(pathlib.Path(temp_dir.name) / "*.nc"),
+        data_vars="minimal",
+        chunks={},
+    )
+    ds_epi.time_bnds.load()
+    delayed_obj = ds_epi.to_netcdf(
+        pathlib.Path(__file__).parents[1] / f"results/{dataset}.nc", compute=False
+    )
+    with dask.diagnostics.ProgressBar():
+        delayed_obj.compute()
+    temp_dir.cleanup()
 
 
 if __name__ == "__main__":
